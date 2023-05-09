f28a7c1556249e146fc85944352050748faffa67
Samuel <101745495+bytutscode@users.noreply.github.com>
2023-05-09 10:06:41 -0300

    Update emailController.ts

--- src/controllers/emailController.ts
+++ src/controllers/emailController.ts
@@ -39,5 +39,5 @@ export const contato = async (req: Request, res: Response) => {
 
     await transport.sendMail(mensageConfig);
 
-    res.redirect('http://www.google.com');
+    res.redirect('https://bytutscode.github.io/portfolio/');
 }
