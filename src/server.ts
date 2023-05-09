fe573f729f61172ee38758fbede2005c75ff5ad7
Samuel <101745495+bytutscode@users.noreply.github.com>
2023-05-09 07:34:01 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,6 @@ const server = express();
 
 server.use(cors());
 
-server.use(express.static(path.join(__dirname, '../public')));
 server.use(express.urlencoded({extended: true}));
 
 server.use(apiRoutes);
