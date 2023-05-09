ffee8b4331dfba2b7a064e6c2ef0fc8d9eb9ce15
Samuel <101745495+bytutscode@users.noreply.github.com>
2023-05-09 07:33:06 -0300

    Update api.ts

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,13 +1,10 @@
 import { Router } from 'express';
-import { Auth } from '../middlewares/auth';
 import * as emailController from '../controllers/emailController';
 
 
 const router = Router();
 
-console.log('oii')
 
 router.get('/', emailController.contato);
-console.log('foi');
 
 export default router;
